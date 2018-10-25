2ef259194a4e999c1f92e59fa9818a4c156a3ca5
hariom <hariom@hariom314673401.Nagarro.local>
2018-10-25 22:33:59 +0530

    update git ignore file

---
+++ src/main/java/com/devops/service/SimpleHello.java
@@ -0,0 +1,24 @@
+package com.devops.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author hrm
+ *
+ */
+public class SimpleHello {
+	
+	/** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleHello.class);
+	/**
+	 * This method is use to print hello message.
+	 */
+	public static void printMessage() {
+		LOGGER.debug("Hello to spring boot");
+	}
+	
+	public static void main(String[] args) {
+		printMessage();
+	}
+}
