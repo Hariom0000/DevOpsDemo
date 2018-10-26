63d877f8a72f10e52b4b9680ccbf3b955d11e1a8
hariom <hariom@hariom314673401.Nagarro.local>
2018-10-27 00:51:07 +0530

    Code formatting

--- src/main/java/com/devops/service/SimpleHello.java
+++ src/main/java/com/devops/service/SimpleHello.java
@@ -8,16 +8,17 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class SimpleHello {
-	
+
 	/** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleHello.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleHello.class);
+
 	/**
 	 * This method is use to print hello message.
 	 */
 	public static void printMessage() {
 		LOGGER.debug("Hello to spring boot");
 	}
-	
+
 	public static void main(String[] args) {
 		printMessage();
 	}
